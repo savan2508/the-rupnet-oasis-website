0d055288add758593df3fe926b04961a083fb71b
savan <sawanpatel2508@gmail.com>
2024-10-03 00:58:50 -0400

    Fix the Header and Navigation

--- app/_components/Navigation.jsx
+++ app/_components/Navigation.jsx
@@ -2,21 +2,33 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <>
-      <ul>
+    <nav className="z-10 text-xl">
+      <ul className="flex gap-16 items-center">
         <li>
-          <Link href={"/"}>Home</Link>
+          <Link
+            href="/cabins"
+            className="hover:text-accent-400 transition-colors"
+          >
+            Cabins
+          </Link>
         </li>
         <li>
-          <Link href={"/cabins"}>Cabins</Link>
+          <Link
+            href="/about"
+            className="hover:text-accent-400 transition-colors"
+          >
+            About
+          </Link>
         </li>
         <li>
-          <Link href={"/account"}>Account</Link>
-        </li>
-        <li>
-          <Link href={"/about"}>About</Link>
+          <Link
+            href="/account"
+            className="hover:text-accent-400 transition-colors"
+          >
+            Guest area
+          </Link>
         </li>
       </ul>
-    </>
+    </nav>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,14 @@ import Navigation from "@/app/_components/Navigation";
 import Logo from "@/app/_components/Logo";
 import "@/app/_styles/globals.css";
 
+import { Josefin_Sans } from "next/font/google";
+import Header from "@/app/_components/Header";
+
+const josefin = Josefin_Sans({
+  display: "swap",
+  subsets: ["latin"],
+});
+
 export const metadata = {
   title: {
     default: "Welcome | The Rupnet Oasis",
@@ -14,11 +22,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
+      <body
+        className={`bg-primary-950 text-primary-100 min-h-screen ${josefin.className}`}
+      >
+        <Header />
         <main>{children}</main>
         <footer>&copy; {new Date().getFullYear()} The Rupnet Oasis</footer>
       </body>
